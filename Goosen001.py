6afc8a1cf443c2c63e0b1640b074b1908ca342d5
apiwat mingmit <120304596+Maxma23891@users.noreply.github.com>
2025-07-25 10:46:19 +0700

    first commit

---
+++ Goosen001.py
@@ -0,0 +1,33 @@
+dicta = [
+{"Goose1" :"ห่านเกรด Economic","Price" :"50 บาท"},
+{"Goose2" :"ห่านเกรด Premium", "Price":"100 บาท"}
+]
+
+
+
+customer = "0"
+print ("ยินดีต้อนรับสู่ห่านย่าง The KMUTT ")
+
+print ("ราคาถูกที่สุดในประเทศไทย ปลอดภัย ")
+
+print("-----------------")
+print("โปรดเลือกห่านเกรดที่ท่านต้องการ:")
+print( dicta )
+
+customer = input("1.ห่าน Premium  เเละ 2.ห่าน Economic")
+
+
+if (customer == "1"):
+   print("ราคา 100 บาทนะคะ")
+   op1 = input("โปรดพิมพ์ Yes:   ")
+   op1 == "Yes" 
+   print ("ดำเนินการ")
+elif (customer == "0" ):
+   print("ราคา 50 บาท นะคะ")
+
+
+
+
+
+
+
